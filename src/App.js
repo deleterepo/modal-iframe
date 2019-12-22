df1cd0433a911f15df5429ad13660d148d74343a
Jordan <jordansherman@live.ca>
2019-12-22 15:57:31 -0500

    Cleanup.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ReactModal from 'react-modal';
+import 'overlayscrollbars/css/OverlayScrollbars.css';
 
 const App = () => {
   const [showModal, setShowModal] = useState(false);
-  const x = undefined;
 
-  console.log(x?.y);
+  ReactModal.setAppElement('#root');
 
   return (
     <>
@@ -15,16 +14,16 @@ const App = () => {
       <ReactModal
         isOpen={showModal}
         contentLabel="Minimal Modal Example"
-        overlayClassName={{ afterOpen: "OverlayAfterOpen", beforeClose: "OverlayBeforeClose"}}
-        className={{ afterOpen: "ModalAfterOpen", beforeClose: "ModalBeforeClose"}}
-        closeTimeoutMS={400}
-        shouldCloseOnOverlayClick 
-        onRequestClose={() => false}
+        overlayClassName={{ base: "base", afterOpen: "OverlayAfterOpen", beforeClose: "OverlayBeforeClose" }}
+        className={{ base: "base", afterOpen: "ModalAfterOpen", beforeClose: "ModalBeforeClose" }}
+        closeTimeoutMS={1000}
+        shouldCloseOnOverlayClick
+        onRequestClose={() => setShowModal(false)}
       >
-        <iframe src={"https://wikipedia.org"} style={{ border: "0px", width: "100%", height: "100%", visibility: showModal ? 'visible' : 'none' }} />
-        <button onClick={() => setShowModal(false)} style={{ position: 'absolute', top: '0px', right: '0px'}}>Close Modal</button>
+          <iframe title="ModalIframeTest" src={"https://wikipedia.org"} style={{ border: "0px", width: "100%", height: "100%", visibility: showModal ? 'visible' : 'none' }} />
+          <button onClick={() => setShowModal(false)} style={{ position: 'absolute', top: '0px', right: '0px' }}>Close Modal</button>
       </ReactModal>
-      <iframe src={"https://wikipedia.org"} style={{ width: '0', height: '0', display: 'none' }} />
+      <iframe title="IframeLoad" src={"https://wikipedia.org"} style={{ width: '0', height: '0', display: 'none' }} />
     </>
   );
 }
