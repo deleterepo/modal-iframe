68a447a3df344325600013287c3e9934df767962
Jordan <jordansherman@live.ca>
2019-12-22 12:08:10 -0500

    Showing modal. Styling modal to look more integrated in the page.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactModal from 'react-modal';
 
-function App() {
+const App = () => {
+  const [showModal, setShowModal] = useState(false);
+  const x = undefined;
+
+  console.log(x?.y);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => setShowModal(true)}>Trigger Modal</button>
+      <ReactModal
+        isOpen={showModal}
+        contentLabel="Minimal Modal Example"
+        overlayClassName="Overlay"
+        className={{ afterOpen: "Modal"}}
+      >
+        <iframe src={"https://wikipedia.org"} style={{ border: "0px", width: "100%", height: "100%", visibility: showModal ? 'visible' : 'none' }} />
+        <button onClick={() => setShowModal(false)} style={{ position: 'absolute', top: '0px', right: '0px'}}>Close Modal</button>
+      </ReactModal>
+      <iframe src={"https://wikipedia.org"} style={{ width: '0', height: '0', display: 'none' }} />
+    </>
   );
 }
 
