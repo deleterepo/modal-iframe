b967b24b03817fe8577c3c4952333f0b473cdac3
Jordan <jordansherman@live.ca>
2019-12-22 16:00:32 -0500

    Removing unused file ref.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
-import './App.css';
-import ReactModal from 'react-modal';
-import 'overlayscrollbars/css/OverlayScrollbars.css';
+import React, { useState } from "react";
+import ReactModal from "react-modal";
+import "./App.css";
 
 const App = () => {
   const [showModal, setShowModal] = useState(false);
 
-  ReactModal.setAppElement('#root');
+  ReactModal.setAppElement("#root");
 
   return (
     <>
@@ -14,18 +13,44 @@ const App = () => {
       <ReactModal
         isOpen={showModal}
         contentLabel="Minimal Modal Example"
-        overlayClassName={{ base: "base", afterOpen: "OverlayAfterOpen", beforeClose: "OverlayBeforeClose" }}
-        className={{ base: "base", afterOpen: "ModalAfterOpen", beforeClose: "ModalBeforeClose" }}
+        overlayClassName={{
+          base: "base",
+          afterOpen: "OverlayAfterOpen",
+          beforeClose: "OverlayBeforeClose"
+        }}
+        className={{
+          base: "base",
+          afterOpen: "ModalAfterOpen",
+          beforeClose: "ModalBeforeClose"
+        }}
         closeTimeoutMS={1000}
         shouldCloseOnOverlayClick
         onRequestClose={() => setShowModal(false)}
       >
-          <iframe title="ModalIframeTest" src={"https://wikipedia.org"} style={{ border: "0px", width: "100%", height: "100%", visibility: showModal ? 'visible' : 'none' }} />
-          <button onClick={() => setShowModal(false)} style={{ position: 'absolute', top: '0px', right: '0px' }}>Close Modal</button>
+        <iframe
+          title="ModalIframeTest"
+          src={"https://wikipedia.org"}
+          style={{
+            border: "0px",
+            width: "100%",
+            height: "100%",
+            visibility: showModal ? "visible" : "none"
+          }}
+        />
+        <button
+          onClick={() => setShowModal(false)}
+          style={{ position: "absolute", top: "0px", right: "0px" }}
+        >
+          Close Modal
+        </button>
       </ReactModal>
-      <iframe title="IframeLoad" src={"https://wikipedia.org"} style={{ width: '0', height: '0', display: 'none' }} />
+      <iframe
+        title="IframeLoad"
+        src={"https://wikipedia.org"}
+        style={{ width: "0", height: "0", display: "none" }}
+      />
     </>
   );
-}
+};
 
 export default App;
