bd10c74f6a2e8a360a82de3dff0917890b945610
Jordan <jordansherman@live.ca>
2019-12-22 12:24:38 -0500

    Adding animations for opening and closing modal.

--- src/App.js
+++ src/App.js
@@ -15,8 +15,11 @@ const App = () => {
       <ReactModal
         isOpen={showModal}
         contentLabel="Minimal Modal Example"
-        overlayClassName="Overlay"
-        className={{ afterOpen: "Modal"}}
+        overlayClassName={{ afterOpen: "OverlayAfterOpen", beforeClose: "OverlayBeforeClose"}}
+        className={{ afterOpen: "ModalAfterOpen", beforeClose: "ModalBeforeClose"}}
+        closeTimeoutMS={400}
+        shouldCloseOnOverlayClick 
+        onRequestClose={() => false}
       >
         <iframe src={"https://wikipedia.org"} style={{ border: "0px", width: "100%", height: "100%", visibility: showModal ? 'visible' : 'none' }} />
         <button onClick={() => setShowModal(false)} style={{ position: 'absolute', top: '0px', right: '0px'}}>Close Modal</button>
